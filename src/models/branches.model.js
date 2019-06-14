6bb16c7132f84dc7ee855f8b79a7415332073b97
Ashish Bhatt <ashishdit2006@gmail.com>
2019-06-14 14:40:41 +0530

    Added banks and branches service

--- src/models/branches.model.js
+++ src/models/branches.model.js
@@ -5,30 +5,32 @@ const DataTypes = Sequelize.DataTypes;
 
 module.exports = function (app) {
   const sequelizeClient = app.get('sequelizeClient');
-  const banks = sequelizeClient.define('banks', {
-    name: {
+  const branches = sequelizeClient.define('branches', {
+    ifsc: {
       type: DataTypes.STRING,
-      allowNull: false
-    },
-    id: {
-      type: DataTypes.INTEGER,
       allowNull: false,
       primaryKey:true
-    }
+    },
+    bank_id:DataTypes.INTEGER,
+    branch: DataTypes.STRING,
+    address: DataTypes.STRING,
+    city: DataTypes.STRING,
+    district: DataTypes.STRING,
+    state: DataTypes.STRING
   }, {
+    timestamps:false,
     hooks: {
       beforeCount(options) {
         options.raw = true;
       }
-    },
-    timestamps: false
+    }
   });
 
   // eslint-disable-next-line no-unused-vars
-  banks.associate = function (models) {
+  branches.associate = function (models) {
     // Define associations here
     // See http://docs.sequelizejs.com/en/latest/docs/associations/
   };
 
-  return banks;
+  return branches;
 };
--- src/services/index.js
+++ src/services/index.js
@@ -1,5 +1,7 @@
 const banks = require('./banks/banks.service.js');
+const branches = require('./branches/branches.service.js');
 // eslint-disable-next-line no-unused-vars
 module.exports = function (app) {
   app.configure(banks);
+  app.configure(branches);
 };
