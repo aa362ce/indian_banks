55479c441ad2186cd47ca4004d35f07bb7537919
Ashish Bhatt <ashishdit2006@gmail.com>
2019-06-14 12:26:58 +0530

    Fixed heroku db config

--- src/sequelize.js
+++ src/sequelize.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
 
 module.exports = function (app) {
-  const connectionString = app.get('DATABASE_URL');
+  const connectionString = process.env.DATABASE_URL
 
   const sequelize = new Sequelize(connectionString, {
     dialect: 'postgres',
